8eb2fcb2beca89a3fcf8c2d60eb0e725c12c4244
Noah Lipsyc <nlipsyc@gmail.com>
2015-10-14 21:05:50 -0400

    Ball now moves at an increasing speed with each pass.

--- easel-source.js
+++ easel-source.js
@@ -1,13 +1,27 @@
 // window.alert("working!");
+var stage, circle;
+var ticker = createjs.Ticker;
+function init(){
+	stage = new createjs.Stage("demoCanvas");
 
 
-var stage = new createjs.Stage("demoCanvas");
-
-var circle = new createjs.Shape();
+	circle = new createjs.Shape();
 	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
 	circle.x = 100;
 	circle.y = 100;
-	stage.addChild(circle)
+	stage.addChild(circle);
 
+
+	ticker.addEventListener("tick", tick);
+	ticker.setFPS(20);
+};
+function tick(event){
+
+	circle.x += 10;
+	if(circle.x>stage.canvas.width){
+		circle.x=0;
+		ticker.setFPS(ticker.getFPS()*1.5);
+	}
 	stage.update();
-	console.log("done!")
+}
+	

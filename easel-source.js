5b20ec787591b69975642b9171c046263074d230
Noah Lipsyc <nlipsyc@gmail.com>
2015-10-16 15:02:05 -0400

    It is now an awful comment strewn mess but, the blue circle drops to half opacity when it connects with the middle of the green square.  Make sure to use localToLocal/localToGlobal with the hitTest. hitTest checks against the local coordinants of the object, not the global ones.

--- easel-source.js
+++ easel-source.js
@@ -1,7 +1,6 @@
 // window.alert("working!");
 var stage, circle, oldX, oldY, strokeColour, strokeWeight,
 	ticker = createjs.Ticker,
-	lastCycle = 0,
 	drawShown = false;
 
 
@@ -21,6 +20,13 @@ function init(){
 	circle.y = 100;
 	stage.addChild(circle);
 
+	square = new createjs.Shape();
+	square.graphics.beginFill("green").drawRect(0,0,25,25);
+	square.x = 0;
+	square.y = 0;
+	stage.addChild(square);
+
+
 	line = new createjs.Shape();
 	stage.addChild(line);
 
@@ -29,87 +35,98 @@ function init(){
 //Mouse events
 
 
-	// circle.on("click", function(){
-	// 	if(circle.x%2===0){
-	// 		circle.x+=51;
-	// 	}
-	// 	else{
-	// 		circle.x-=51;
-	// 	}
-	// 	stage.update();
-	// });
-	// circle.on("mouseover", function(event){
-	// 	console.log("mouseover");
-	// 	circle.graphics.clear().beginFill("purple").drawCircle(0,0,50);
-	// 	stage.update();
-	// });
-	// circle.on("mouseout", function(event){
-	// 	console.log("mouseout");
-	// 	circle.graphics.clear().f("DeepSkyBlue").dc(0,0,50);
-	// 	stage.update();
-	// });
+		// circle.on("click", function(){
+		// 	if(circle.x%2===0){
+		// 		circle.x+=51;
+		// 	}
+		// 	else{
+		// 		circle.x-=51;
+		// 	}
+		// 	stage.update();
+		// });
+		// circle.on("mouseover", function(event){
+		// 	console.log("mouseover");
+		// 	circle.graphics.clear().beginFill("purple").drawCircle(0,0,50);
+		// 	stage.update();
+		// });
+		// circle.on("mouseout", function(event){
+		// 	console.log("mouseout");
+		// 	circle.graphics.clear().f("DeepSkyBlue").dc(0,0,50);
+		// 	stage.update();
+		// });
 
-	circle.on("pressmove", function(evt){
-		evt.currentTarget.x = evt.stageX-50;
+	square.on("pressmove", function(evt){
+		evt.currentTarget.x = evt.stageX;
 		evt.currentTarget.y = evt.stageY;
 
 		stage.update();
 	});
-	circle.on("click", function(event){
-		console.log("click!");
-		strokeColour = createjs.Graphics.getHSL(Math.random()*360, 100, 50);
-		strokeWeight = 20*Math.random();
+	// circle.on("click", function(event){
+	// 	console.log("click!");
+	// 	strokeColour = createjs.Graphics.getHSL(Math.random()*360, 100, 50);
+	// 	strokeWeight = 20*Math.random();
 
-		handleDraw();
-	});
+	// 	handleDraw();
+	// });
 
 
-	// ticker.addEventListener("tick", tick);
-	// ticker.setFPS(20);
+	ticker.addEventListener("tick", tick);
+	ticker.setFPS(20);
 }
 
 
-/**********Drawing*******/
+// /**********Drawing*******/
 
 
-function handleDraw(){
-	drawShown=!drawShown;
+	// function handleDraw(){
+	// 	drawShown=!drawShown;
 
-	console.log(drawShown);
+	// 	console.log(drawShown);
 
-	stage.on("stagemousemove", function(evt){
+	// 	stage.on("stagemousemove", function(evt){
 
-		if(oldX && drawShown){
-			line.graphics.beginStroke(strokeColour)
-							.setStrokeStyle(strokeWeight,"round")
-							.moveTo(oldX,oldY)
-							.lineTo(evt.stageX, evt.stageY);
-			stage.update();
-		}
-		oldX = evt.stageX;
-		oldY = evt.stageY;
-	});
-	stage.update();
-}
+	// 		if(oldX && drawShown){
+	// 			line.graphics.beginStroke(strokeColour)
+	// 							.setStrokeStyle(strokeWeight,"round")
+	// 							.moveTo(oldX,oldY)
+	// 							.lineTo(evt.stageX, evt.stageY);
+	// 			stage.update();
+	// 		}
+	// 		oldX = evt.stageX;
+	// 		oldY = evt.stageY;
+	// 	});
+	// 	stage.update();
+	// }
 
 /***********Animation************/
-// function tick(event){
-// 	circle.x += event.delta/1000*200;
-// 	if(circle.x>stage.canvas.width){
-
-// 	console.log("delta", event.delta);
-// 	console.log("fps", ticker.getFPS());
-// 	console.log("cycle time", ticker.getTime()-lastCycle);
-// 	lastCycle = ticker.getTime();
+function tick(event){
+			//ms since last tick / ms per s * px per sec
+	circle.alpha = 1;
+	circle.x += event.delta/1000*10;
+	if(circle.x>stage.canvas.width){
 
 
-// 		circle.x=0;
-// 		ticker.setFPS(ticker.getFPS()*1.5);
 
-// 		if(ticker.getFPS()>80){
-// 			ticker.setFPS(20);
-// 		}
-// 	}
-// 	stage.update();
-// }
+		circle.x=0;
+			//increases fps with each cycle
+			//ticker.setFPS(ticker.getFPS()*1.5);
+
+			// if(ticker.getFPS()>80){
+				// ticker.setFPS(20);
+		// }
+	}
+	//Hit test
+	
+	var pt = square.localToLocal(12, 12, circle);
+
+	console.log("square", pt.x, pt.y);
+	console.log("circle", circle.x, circle.y);
+	
+	if (circle.hitTest(pt.x, pt.y)){
+		console.log('hit');
+		circle.alpha = 0.5;
+	}
+
+	stage.update();
+}
 	
